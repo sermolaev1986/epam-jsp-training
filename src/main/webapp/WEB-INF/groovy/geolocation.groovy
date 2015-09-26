17dbf27c67ee78ba45979c747e535ce3c20e706d
sermolaev <sermolaev.1986@gmail.com>
2015-09-27 02:58:48 +0400

    - fixed url encoding of address

--- src/main/webapp/WEB-INF/groovy/geolocation.groovy
+++ src/main/webapp/WEB-INF/groovy/geolocation.groovy
@@ -1,7 +1,7 @@
 def api_key = 'AIzaSyBwGDozCsY1vb2DR8cHxYsWgty7mb_5LKs'
 def google_api_url = 'https://maps.googleapis.com/maps/api/geocode/xml'
 
-def address = params.address
+def address = java.net.URLEncoder.encode(params.address,'UTF-8')
 def response = "$google_api_url?address=$address&$api_key".toURL().text
 response = new XmlSlurper().parseText(response)
 println "Formatted address: $response.result.formatted_address"
